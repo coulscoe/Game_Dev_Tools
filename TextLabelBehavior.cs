5d0e748407e30ad3581ae4b2d8da09c08c2fc779
Coen Coulson <143137210+coulscoe@users.noreply.github.com>
2024-08-26 18:57:19 -0600

    Create TextLabelBehavior.cs

---
+++ TextLabelBehavior.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+
+public class TextLabelBehavior : MonoBehaviour
+{
+    public Text label;
+    public FloatData dataObj;
+    private void Start()
+    {
+        label = GetComponent<Text>();
+        UpdateLabel();
+    }
+
+    public void UpdateLabel()
+    {
+        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
